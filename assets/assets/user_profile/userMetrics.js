96a0d0263baa8fb8d2e9b2df3e28235e77f41f14
Juliette Parmentier <juliette.parmentier@unamur.be>
2024-12-05 21:11:57 +0100

    Delete books by category

--- assets/assets/user_profile/userMetrics.js
+++ assets/assets/user_profile/userMetrics.js
@@ -4,12 +4,27 @@ export function userMetrics() {
 
     if (pageStatistics.textContent == "Statistiques") {
         pageStatistics.remove()
+        pageStatistics.classList.add("processed-by-script");
     }
 
     // Reading challenge, nb of critics, badges, etc.
     const sideStatistics = document.querySelector("#page_corps > div > div.side_r > div:nth-child(1)")
     if (sideStatistics) {
         sideStatistics.remove()
+        sideStatistics.classList.add("processed-by-script");
     }
+
+    // Number of books by category on profile (example : "Littérature italienne")
+    const booksByCategories = document.querySelectorAll("#page_corps > div > div.side_l > div > div.liste_fiches > div")
+
+    if (booksByCategories) {
+        const nbBooksByCategory = document.querySelectorAll("a > div > p")
+
+        nbBooksByCategory.forEach(nbByCategory => {
+            nbByCategory.remove();
+            nbByCategory.classList.add("processed-by-script");
+        });
+    };
+
 
 }
