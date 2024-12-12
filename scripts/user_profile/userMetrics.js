465d9de2ed8375beb7a8416659c5593732692db6
Juliette Parmentier <juliette.parmentier@unamur.be>
2024-12-12 14:53:17 +0100

    Fix side panel deletion

--- scripts/user_profile/userMetrics.js
+++ scripts/user_profile/userMetrics.js
@@ -29,17 +29,22 @@ export function userMetrics() {
     });
 
     // Insignes et contributions
-    const insigneBanner = document.querySelector("#page_corps > div > div.side_r > div > div:nth-child(7)");
+    // const insigneBanner = document.querySelector("#page_corps > div > div.side_r > div > div:nth-child(7)");
 
-    if (insigneBanner && insigneBanner.textContent.includes("insignes")) {
-        insigneBanner.remove();
+    // if (insigneBanner && insigneBanner.textContent.includes("insignes")) {
+    //     insigneBanner.remove();
     
-        const sidePanel = document.querySelector("div.side_r");
-        const allSeparators = sidePanel.querySelectorAll("div.sep");
-    
-        if (allSeparators.length > 1) {
-            allSeparators[1].remove();  // Supprimer le second élément (index 1)
-        }
+
+    //     const allSeparators = sidePanel.querySelectorAll("div.sep");
+    //     if (allSeparators.length > 1) {
+    //         allSeparators[1].remove();  // Supprimer le second élément (index 1)
+    //     }
+    // }
+
+    const sidePanel = document.querySelector("div.side_r_content");
+
+    if(sidePanel) {
+        sidePanel.remove()
     }
     
 
