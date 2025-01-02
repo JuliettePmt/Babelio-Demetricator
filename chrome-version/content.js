49944b6f43795b43c24028c28f69468a426bf90e
Juliette Parmentier <juliette.parmentier@unamur.be>
2025-01-02 18:34:47 +0100

    Various improvments (including readme)

--- chrome-version/content.js
+++ chrome-version/content.js
@@ -2,15 +2,8 @@ import { ratings } from "./scripts/book_pages/ratings.js";
 import { community } from "./scripts/book_pages/community.js";
 import { platformMetrics } from "./scripts/book_pages/platformMetrics.js";
 import { commercial } from "./scripts/book_pages/commercial.js";
-import { userMetrics } from "./scripts/user_profile/userMetrics.js";
+// import { userMetrics } from "./scripts/user_profile/userMetrics.js";
 
-function safeExecute(fn, scriptName) {
-    try {
-        fn();
-    } catch (error) {
-        console.error(`Error in ${scriptName} script:`, error);
-    }
-}
 
 // BOOK PAGES
 ratings();
@@ -19,7 +12,7 @@ platformMetrics();
 commercial();
 
 // USER PROFILE
-userMetrics();
+// userMetrics();
 
 
 // Execute : npm run build
--- firefox-version/readme.md
+++ firefox-version/readme.md
@@ -3,9 +3,11 @@
 
 1. Execute ```npm run build``` in the root folder. 
    
-2. Launch ```createFirefoxVersion.py``` script
-
 
 ---------
 The script will take all files from the Chrome version, except for :
 - manifest.json (but the version will still be changed to match the Chrome manifest version, for example : 2.9.0)
+
+---------
+Run local test version :
+```web-ext build``` in the **'firefox-version/firefox-plugin' folder** !
