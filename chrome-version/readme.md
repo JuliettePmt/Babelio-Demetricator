e5922be0fec2802ad62c1bdcd2c81ae21d1bb4cb
Juliette Parmentier <juliette.parmentier@unamur.be>
2025-05-13 15:50:00 +0200

    Bien avancé : Modification effective et propre des messages + suppression d'éléments sur la page Accueil du profil, barre de droite

---
+++ chrome-version/readme.md
@@ -0,0 +1,3 @@
+# How to generate the Chrome version
+
+Execute 'npx webpack' in the Chrome folder.
--- chrome-version/scripts/user_profile/userMetrics.js
+++ chrome-version/scripts/user_profile/userMetrics.js
@@ -2,32 +2,99 @@ export function userMetrics() {
   console.log("userMetrics OK");
 
   // *** Volet "Statistiques"
-    const pageStatistics = document.querySelector(
-      "#page_corps > div > div.livre_header.row > div > div > a:nth-child(4)"
-    );
+    const pageStatistics = document.querySelector("#page_corps > div > div.livre_header.row > div > div > a:nth-child(4)");
     if (pageStatistics) {
       if (pageStatistics.textContent == "Statistiques") {
         pageStatistics.remove();
       }
     }
 
-    // Nb d'abonnés (première page)
-    const nbFollowers = document.querySelectorAll('a')
-    if (nbFollowers.length > 0) {
-      nbFollowers.forEach(e.id.startsWith("abonnes"))
-      e.forEach((link) => {
-        link.remove();
-      });
-    }
+    // Nb d'abonnés et d'abonnements (page d'accueil du profil)
+    const observer = new MutationObserver(() => {
 
-    // Nb d'abonnements (première page)
-    const nbFollowing = document.querySelectorAll('a[href="/abonnements');
+      var messageElement = document.querySelector('.actualites_side_item');
+      if (messageElement) { // RESTE : À SUPPRIMER LA BALISE FONT ET "Message(s)"
+        const messages = document.querySelector("a.actualites_side_item > font");
 
-    if (nbFollowing.length > 0) {
-      nbFollowing.forEach((link) => {
-        link.remove();
-      });
-    }
+        if (messages) { // Delete the number of messages
+          let numberOfMessages = parseInt(messages.textContent.trim(), 10); // Convertir en nombre
+          let message = "Vous n'avez pas de message";
+    
+          if (numberOfMessages === 1) {
+            message = "Vous avez un message";
+          }
+
+          else if (numberOfMessages > 1) {
+            message = "Vous avez des messages";
+          }
+
+          else if (numberOfMessages === 0) {
+            message = "Vous n'avez pas de message";
+          }
+
+          messages.remove();
+          messageElement.innerHTML = messageElement.innerHTML.replace('Message(s)', message);
+
+        }       
+      }
+
+      const nbFollowers = document.querySelectorAll('a[href="/abonnes"]');
+      const nbFollowing = document.querySelectorAll('a[href="/abonnements"]');
+
+      if (nbFollowers) {
+        nbFollowers.forEach(function(follower) {
+          follower.remove();
+        });
+      }
+
+      if (nbFollowing) {
+        nbFollowing.forEach(function(following) {
+          following.remove();
+        });
+      }
+      observer.disconnect(); // Eviter boucle infinie
+
+
+      const sideStats = document.getElementsByClassName("side_stats");
+      if (sideStats) {
+        // Convert HTMLCollection to an array using Array.from
+        Array.from(sideStats).forEach((stat) => {
+          stat.remove()
+        });
+      }
+
+      // "Contribution sur XX% de vos livres"
+      const contributionStat = document.getElementsByClassName("contribution_progress"); 
+      if (contributionStat) {
+        // Convert HTMLCollection to an array using Array.from or spread operator
+        Array.from(contributionStat).forEach((stat) => {
+          stat.remove()
+        });
+      }
+
+      const contributionStatLegend = document.getElementsByClassName("contribution_legend"); 
+      if (contributionStatLegend) {
+        // Convert HTMLCollection to an array using Array.from or spread operator
+        Array.from(contributionStatLegend).forEach((stat) => {
+          stat.remove()
+        });
+      }
+      
+      // "Contributions appréciées"
+      const titres = document.getElementsByClassName("side_r_content"); 
+      if (titres) {
+        // Convert HTMLCollection to an array using Array.from or spread operator
+        Array.from(titres).forEach((titre) => {
+          if (titre.textContent.includes("Contributions appréciées") ||titre.textContent.includes("Notez vos lectures")) {
+            titre.remove()
+          }
+        });
+      }
+
+    });
+    observer.observe(document.body, { childList: true, subtree: true });
+  
+
 
   //   // Insignes et contributions (sur la page Accueil du profil)
     const insigneBanner = document.querySelector(
